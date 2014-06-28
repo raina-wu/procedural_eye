57ac64167a1a8016807d21504ce06b130118b150
raina-wu <RR@xiaoyu-macbookpro.local>
2014-06-29 01:37:58 +1000

    fix namespace problem. supports adding multiple instances to one scene

--- eyeball.py
+++ eyeball.py
@@ -69,7 +69,12 @@ def changeIrisSize(*args):
 
     
 def reset(*args):
-    print 'enter reset'
+    selected=cmds.ls(selection=True)
+    if len(selected)!=1 or cmds.nodeType(selected[0])!='locator':
+        raise RuntimeError, 'Please select one eyeControler.'
+    else:
+        print 'enter reset, selected:'
+        print selected
 
 
 
@@ -85,19 +90,10 @@ def linstep(start, end, para):
 
 def buildEyeballGeo():  
 
-# #add namespace
-#     cnt=1
-#     global gNamespace
-#     while cmds.namespace(exists=gNamespace):
-#         cnt=cnt+1
-#         gNamespace='eyeball'+str(cnt)
-#     cmds.namespace(add=gNamespace)
-#     cmds.namespace(set=gNamespace)
-
 #create eyeball controler-----------------------------------------------------------------------------------------
 
-    gEyeballCtrler='eyeballCtrl'
-    cmds.spaceLocator(name=gEyeballCtrler)
+
+    gEyeballCtrler=cmds.spaceLocator()[0]
     cmds.addAttr(longName='pupilSize', attributeType='float', keyable=True, defaultValue=0.7)
     cmds.addAttr(longName='irisSize', attributeType='float', keyable=True, defaultValue=2.75)
     cmds.addAttr(longName='irisConcave', attributeType='float', keyable=True, defaultValue=10)
@@ -107,74 +103,69 @@ def buildEyeballGeo():
 #cornea-----------------------------------------------------------------------------------------
 
     #create eyeball base geometry and detach
-    cmds.sphere(name='eyeballSphere', sections=20, spans=20, axis=(0,0,1), radius=0.5)     
-    pieceNames = cmds.detachSurface('eyeballSphere', ch=1, rpo=1, parameter=(0.1,20))
-    cmds.rename(pieceNames[0],'corneaGeo')
-    cmds.rename(pieceNames[2],'corneaDetach')
-    cmds.parent('eyeballSphere', gEyeballCtrler, relative=True)
-    cmds.parent('corneaGeo', gEyeballCtrler, relative=True)
+    eyeballSphere=cmds.sphere(sections=20, spans=20, axis=(0,0,1), radius=0.5)[0]
+#     eyeballSphere=eyeballSphere[0]
+    pieceNames = cmds.detachSurface(eyeballSphere, ch=1, rpo=1, parameter=(0.1,20))
+    corneaGeo=pieceNames[0];
+    corneaDetach=pieceNames[2];
+    cmds.parent(eyeballSphere, gEyeballCtrler, relative=True)
+    cmds.parent(corneaGeo, gEyeballCtrler, relative=True)
 
     #add lattice and deform cornea
-    cmds.select('corneaGeo')
-    latticeNames = cmds.lattice(dv=(2,2,6), oc=False)
-    cmds.rename(latticeNames[0], 'corneaLat')
-    cmds.rename(latticeNames[1], 'corneaLatGeo')
-    cmds.rename(latticeNames[2], 'corneaLatGeoBase')
-    cmds.setAttr('corneaLatGeo.scale', 1.1, 1.1, 1.1)
-    cmds.setAttr('corneaLatGeoBase.scale', 1.1, 1.1, 1.1)
+    cmds.select(corneaGeo)
+    (corneaLat,corneaLatGeo,corneaLatGeoBase)= cmds.lattice(dv=(2,2,6), oc=False)
+    cmds.setAttr(corneaLatGeo+'.scale', 1.1, 1.1, 1.1)
+    cmds.setAttr(corneaLatGeoBase+'.scale', 1.1, 1.1, 1.1)
     
-    cmds.createNode('transform', name='corneaDetachRider')
-    cmds.createNode('transform', name='corneaDeformGrp')
-    cmds.createNode('transform', name='corneaRadius')
+    corneaDetachRider=cmds.createNode('transform')
+    corneaDeformGrp=cmds.createNode('transform')
+    corneaRadius=cmds.createNode('transform')
             
-    cmds.parent('corneaDeformGrp', 'corneaDetachRider', relative=True)
-    cmds.parent('corneaRadius', 'corneaDetachRider', relative=True)
-    cmds.parent('corneaLatGeo', 'corneaDeformGrp', relative=True)
-    cmds.parent('corneaLatGeoBase', 'corneaDeformGrp', relative=True)
-    cmds.parent('corneaDetachRider', gEyeballCtrler, relative=True)
+    cmds.parent(corneaDeformGrp, corneaDetachRider, relative=True)
+    cmds.parent(corneaRadius, corneaDetachRider, relative=True)
+    cmds.parent(corneaLatGeo, corneaDeformGrp, relative=True)
+    cmds.parent(corneaLatGeoBase, corneaDeformGrp, relative=True)
+    cmds.parent(corneaDetachRider, gEyeballCtrler, relative=True)
     
-    cmds.hide('corneaDetachRider')
+    cmds.hide(corneaDetachRider)
     
     
 #iris-----------------------------------------------------------------------------------------
 
     #create eyeball base geometry and detach
-    cmds.sphere(name='eyeballSphere2', sections=20, spans=20, axis=(0,0,1), radius=0.5)     
-    pieceNames = cmds.detachSurface('eyeballSphere2', ch=1, rpo=1, parameter=(0.1,20))
-    cmds.rename(pieceNames[0],'irisGeo')
-    cmds.rename(pieceNames[2],'irisDetach')
-    cmds.delete('eyeballSphere2')
-    cmds.parent('irisGeo', gEyeballCtrler, relative=True)
+    eyeballSphere2=cmds.sphere(sections=20, spans=20, axis=(0,0,1), radius=0.5)[0]
+    pieceNames = cmds.detachSurface(eyeballSphere2, ch=1, rpo=1, parameter=(0.1,20))
+    irisGeo=pieceNames[0]
+    irisDetach=pieceNames[2]
+    cmds.delete(eyeballSphere2)
+    cmds.parent(irisGeo, gEyeballCtrler, relative=True)
 
     #add lattice and deform iris
-    cmds.select('irisGeo')
-    latticeNames = cmds.lattice(dv=(2,2,2), oc=False)
-    cmds.rename(latticeNames[0], 'irisLat')
-    cmds.rename(latticeNames[1], 'irisLatGeo')
-    cmds.rename(latticeNames[2], 'irisLatGeoBase')
-    cmds.setAttr('irisLatGeo.scale', 1.1, 1.1, -2)
-    cmds.setAttr('irisLatGeoBase.scale', 1.1, 1.1, 2)
-    cmds.setAttr('irisLatGeo.translateZ', -0.5)
-    cmds.setAttr('irisLatGeoBase.translateZ', -0.5)
+    cmds.select(irisGeo)
+    (irisLat,irisLatGeo,irisLatGeoBase) = cmds.lattice(dv=(2,2,2), oc=False)
+    cmds.setAttr(irisLatGeo+'.scale', 1.1, 1.1, -2)
+    cmds.setAttr(irisLatGeoBase+'.scale', 1.1, 1.1, 2)
+    cmds.setAttr(irisLatGeo+'.translateZ', -0.5)
+    cmds.setAttr(irisLatGeoBase+'.translateZ', -0.5)
     
-    cmds.createNode('transform', name='irisDetachRider')
-    cmds.createNode('transform', name='irisDeformGrp')
-    cmds.createNode('transform', name='irisRadius')
+    irisDetachRider=cmds.createNode('transform')
+    irisDeformGrp=cmds.createNode('transform')
+    irisRadius=cmds.createNode('transform')
             
-    cmds.parent('irisDeformGrp', 'irisDetachRider', relative=True)
-    cmds.parent('irisRadius', 'irisDetachRider', relative=True)
-    cmds.parent('irisLatGeo', 'irisDeformGrp', relative=True)
-    cmds.parent('irisLatGeoBase', 'irisDeformGrp', relative=True)
-    cmds.parent('irisDetachRider', gEyeballCtrler, relative=True)
+    cmds.parent(irisDeformGrp, irisDetachRider, relative=True)
+    cmds.parent(irisRadius, irisDetachRider, relative=True)
+    cmds.parent(irisLatGeo, irisDeformGrp, relative=True)
+    cmds.parent(irisLatGeoBase, irisDeformGrp, relative=True)
+    cmds.parent(irisDetachRider, gEyeballCtrler, relative=True)
 
-    cmds.hide('irisDetachRider')
+    cmds.hide(irisDetachRider)
     
 #pupil-----------------------------------------------------------------------------------------
     #detach from iris geometry       
-    pieceNames = cmds.detachSurface('irisGeo', ch=1, rpo=1, parameter=(0.1,20))
-    cmds.rename(pieceNames[0], 'pupilGeo')
-    cmds.rename(pieceNames[2], 'pupilDetach')
-    cmds.parent('pupilGeo', gEyeballCtrler, relative=True)
+    pieceNames = cmds.detachSurface(irisGeo, ch=1, rpo=1, parameter=(0.1,20))
+    pupilGeo=pieceNames[0]
+    pupilDetach=pieceNames[2]
+    cmds.parent(pupilGeo, gEyeballCtrler, relative=True)
     
     
 #connect attributes-----------------------------------------------------------------------------------------
@@ -187,28 +178,28 @@ def buildEyeballGeo():
                     float $cornea_rad  = (sin(deg_to_rad('''+gEyeballCtrler+'''.irisSize*9.0))) * 0.5;
                     //define nurbs surface cornea detach position
                     float $cornea_par  = ((1.0 - linstep( 0.0,10.0,'''+gEyeballCtrler+'''.irisSize)) * 10.0 ) + 10.0;
-                    corneaDetach.parameter[0] = $cornea_par;
-                    corneaDetachRider.translateZ = $cornea_tz;
-                    corneaRadius.translateX = $cornea_rad;
-                    corneaDeformGrp.translateZ = ( 0.5 - $cornea_tz ) * 0.5;
-                    corneaDeformGrp.scaleX = corneaDeformGrp.scaleY = $cornea_rad * 2.0;
-                    corneaDeformGrp.scaleZ = ( 0.5 - $cornea_tz );
-                    corneaLat.envelope = (1.0 - (smoothstep(0.0,11.0,'''+gEyeballCtrler+'''.irisSize))) * ('''+gEyeballCtrler+'''.corneaBulge * 0.1);
+                    '''+corneaDetach+'''.parameter[0] = $cornea_par;
+                    '''+corneaDetachRider+'''.translateZ = $cornea_tz;
+                    '''+corneaRadius+'''.translateX = $cornea_rad;
+                    '''+corneaDeformGrp+'''.translateZ = ( 0.5 - $cornea_tz ) * 0.5;
+                    '''+corneaDeformGrp+'''.scaleX = '''+corneaDeformGrp+'''.scaleY = $cornea_rad * 2.0;
+                    '''+corneaDeformGrp+'''.scaleZ = ( 0.5 - $cornea_tz );
+                    '''+corneaLat+'''.envelope = (1.0 - (smoothstep(0.0,11.0,'''+gEyeballCtrler+'''.irisSize))) * ('''+gEyeballCtrler+'''.corneaBulge * 0.1);
                     
                     //calculate iris-related parameters
                     float $iris_tz   = ( cos(deg_to_rad(('''+gEyeballCtrler+'''.irisSize+0.3)*9.0)) ) * 0.485;
                     float $iris_rad  = ( sin(deg_to_rad(('''+gEyeballCtrler+'''.irisSize+0.3)*9.0)) ) * 0.485;
                     float $iris_par  = ((1.0 - (('''+gEyeballCtrler+'''.irisSize+0.3)*0.1)) * 10.0 ) + 10.0;
-                    irisDetach.parameter[0] = $iris_par;
-                    irisDetachRider.translateZ = $iris_tz;
-                    irisRadius.translateX = $iris_rad;
-                    irisDeformGrp.translateZ = ( 0.5 - $iris_tz ) * 0.5;
-                    irisDeformGrp.scaleX = irisDeformGrp.scaleY = $iris_rad * 2.0;
-                    irisDeformGrp.scaleZ = ( 0.5 - $iris_tz );
-                    irisLat.envelope = '''+gEyeballCtrler+'''.irisConcave * 0.1;
+                    '''+irisDetach+'''.parameter[0] = $iris_par;
+                    '''+irisDetachRider+'''.translateZ = $iris_tz;
+                    '''+irisRadius+'''.translateX = $iris_rad;
+                    '''+irisDeformGrp+'''.translateZ = ( 0.5 - $iris_tz ) * 0.5;
+                    '''+irisDeformGrp+'''.scaleX = '''+irisDeformGrp+'''.scaleY = $iris_rad * 2.0;
+                    '''+irisDeformGrp+'''.scaleZ = ( 0.5 - $iris_tz );
+                    '''+irisLat+'''.envelope = '''+gEyeballCtrler+'''.irisConcave * 0.1;
                     
                     float $pupil_par = max( (((1.0 - (('''+gEyeballCtrler+'''.pupilSize)*0.1)) * 10.0 ) + 10.0), $iris_par + 0.1 );
-                    pupilDetach.parameter[0] = $pupil_par;
+                    '''+pupilDetach+'''.parameter[0] = $pupil_par;
                     '''
     
     cmds.expression(s=expressionStr)
@@ -217,10 +208,36 @@ def buildEyeballGeo():
     for x in range(0,2):
         for y in range(0,2):
             for z in range(3,6):
-                pointNameStr='corneaLatGeo.pt['+str(x)+']['+str(y)+']['+str(z)+']'
+                pointNameStr=corneaLatGeo+'.pt['+str(x)+']['+str(y)+']['+str(z)+']'
                 cmds.select(pointNameStr)
                 cmds.move(0, 0, 0.05, r=True)
-    
-    cmds.namespace(set=':')
+ 
+#rename objects------------------------------------
 
+    cnt=1
+    gEyeballCtrlerName='eyeballCtrl'+str(cnt)
+    while cmds.namespace(exists=gEyeballCtrlerName):
+        cnt=cnt+1
+        gEyeballCtrlerName='eyeballCtrl'+str(cnt)
     
+    cmds.rename(gEyeballCtrler, gEyeballCtrlerName)
+    cmds.rename(pupilGeo, 'pupilGeo')
+    cmds.rename(pupilDetach, 'pupilDetach')
+    
+    cmds.rename(irisLat, 'irisLat')
+    cmds.rename(irisLatGeo, 'irisLatGeo')
+    cmds.rename(irisLatGeoBase, 'irisLatGeoBase')
+    cmds.rename(irisGeo,'irisGeo')
+    cmds.rename(irisDetach,'irisDetach')
+    cmds.rename(irisDetachRider,'irisDetachRider')
+    cmds.rename(irisDeformGrp,'irisDeformGrp')
+    cmds.rename(irisRadius,'irisRadius')
+    cmds.rename(corneaLat, 'corneaLat')
+    cmds.rename(corneaLatGeo, 'corneaLatGeo')
+    cmds.rename(corneaLatGeoBase, 'corneaLatGeoBase')
+    cmds.rename(eyeballSphere,'eyeballSphere')
+    cmds.rename(corneaGeo,'corneaGeo')
+    cmds.rename(corneaDetach,'corneaDetach')
+    cmds.rename(corneaDetachRider,'corneaDetachRider')
+    cmds.rename(corneaDeformGrp,'corneaDeformGrp')
+    cmds.rename(corneaRadius,'corneaRadius')
